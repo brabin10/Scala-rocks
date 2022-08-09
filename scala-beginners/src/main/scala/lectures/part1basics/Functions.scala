22c0cb0f5324abeea325224ef9a665836fa2a481
Rabin Basnet <rowbinr@yahoo.com>
2022-08-08 18:44:06 -0600

    some function exercises to do

--- scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
+++ scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
@@ -9,18 +9,38 @@ object Functions extends App {
   println(aFunction("hello", 3))
 
   def aParameterlessFunction(): Int = 42
+
   println(aParameterlessFunction())
   println(aParameterlessFunction)
 
   def aRepeatedFunction(aString: String, n: Int): String = {
     if (n == 1) aString
-    else aString + aRepeatedFunction(aString, n-1)
+    else aString + aRepeatedFunction(aString, n - 1)
   }
 
-  println(aRepeatedFunction("hello",3))
+  println(aRepeatedFunction("hello", 3))
 
   // WHEN YO NEED LOOPS, USE RECURSION
 
+  def aFunctionWithSideEffects(aString: String): Unit = println(aString)
+
+  def aBigFunction(n: Int): Int = {
+    def aSmallerFunction(a: Int, b: Int): Int = a + b
+
+    aSmallerFunction(n, n - 1)
+
+  }
+
+  /*
+    1. A greeting function (name, age) => "Hi, my name is $name and I am $age years old."
+    2. Factorial function 1 * 2 * 3 * .. * n
+    3. A Fibonacci function
+       f(1) = 1
+       f(2) = 1
+       f(n) = f(n - 1) + f(n - 2)
+    4. Tests if a number is prime.
+
+    */
 
 
 }
