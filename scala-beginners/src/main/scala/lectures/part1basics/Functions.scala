dd1a09b17101c9156ac9396d50984c5e780f3aed
Rabin Basnet <rowbinr@yahoo.com>
2022-08-08 18:27:49 -0600

    when you need loops, use recursion

--- scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
+++ scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
@@ -2,4 +2,25 @@ package lectures.part1basics
 
 object Functions extends App {
 
+  def aFunction(a: String, b: Int): String = {
+    a + " " + b
+  }
+
+  println(aFunction("hello", 3))
+
+  def aParameterlessFunction(): Int = 42
+  println(aParameterlessFunction())
+  println(aParameterlessFunction)
+
+  def aRepeatedFunction(aString: String, n: Int): String = {
+    if (n == 1) aString
+    else aString + aRepeatedFunction(aString, n-1)
+  }
+
+  println(aRepeatedFunction("hello",3))
+
+  // WHEN YO NEED LOOPS, USE RECURSION
+
+
+
 }
