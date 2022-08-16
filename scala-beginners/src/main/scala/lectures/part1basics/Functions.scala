6af62ab1aafbc9dea3049f4b8e95b4c40632ceeb
Rabin Basnet <rowbinr@yahoo.com>
2022-08-15 21:18:45 -0600

    learn different functions in scala

--- scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
+++ scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
@@ -42,5 +42,34 @@ object Functions extends App {
 
     */
 
+  def greetingForKids(name: String, age: Int): String =
+    "Hi, my name is " + name + " and I am " + age + " years old."
+    println(greetingForKids("Harry", 21))
+
+  def factorial(n: Int): Int =
+    if (n <= 0) 1
+    else n * factorial(n-1)
+
+  println(factorial(5))
+
+  def fibonacci(n: Int): Int =
+    if(n <= 1) 1
+    else fibonacci(n-1) + fibonacci(n-2)
+  // 1 1 2 3 5 8 13 21 fibonacci numbers
+  println(fibonacci(8))
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t: Int): Boolean =
+      if(t <= 1) true
+      else n % t != 0 && isPrimeUntil(t-1)
+
+    isPrimeUntil(n/2)
+
+  }
+  println(isPrime(23))
+  println(isPrime(2922))
+  println(isPrime(32 *33))
+
+
 
 }
